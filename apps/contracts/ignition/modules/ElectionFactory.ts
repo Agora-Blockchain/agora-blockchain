163ee561c0743f7a612221ec8d1ec11a7e559fee
Aditya Bhattad <adityabhattad18@gmail.com>
2024-05-15 17:19:39 +0530

    setup(contracts): initialize a hardhat project

---
+++ apps/contracts/ignition/modules/ElectionFactory.ts
@@ -0,0 +1,11 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+
+
+const ElectionFactoryModule = buildModule("ElectionFactoryModule", (m) => {
+    const electionFactory = m.contract("ElectionFactory", [], {});
+
+    return { electionFactory };
+})
+
+export default ElectionFactoryModule;
