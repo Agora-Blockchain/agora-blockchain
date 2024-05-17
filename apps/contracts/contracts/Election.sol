09428ca1b42cb4823737a443c5cfca9db5399082
Aditya Bhattad <adityabhattad18@gmail.com>
2024-05-17 11:07:40 +0530

    feat(contracts): add election contract

--- apps/contracts/contracts/Election.sol
+++ apps/contracts/contracts/Election.sol
@@ -22,9 +22,59 @@ pragma solidity ^0.8.20;
 // Private function
 // View and Pure functions
 
+import {IBallot} from "./ballots/IBallot.sol";
+import {IResultCalculator} from "./resultCalculators/IResultCalculator.sol";
+
 
 contract Election {
 
-    
+    error Election__FailedToAddCandidate();
+    error Election__ResultsNotDeclared();
+
+    uint256[] private candidateList;
+    mapping(uint256 => uint256) private candidateVotes;
+
+    IBallot private ballotAddress;
+    IResultCalculator private resultCalculatorAddress;
+    uint256 private winnerId;
+    bool private resultDeclared;
+
+    event CandidateAdded(uint256 candidateId);
+    event Voted(uint256 candidateId);
+    event ResultDeclared(uint256 winnerId);
+
+    constructor(address _ballotAddress, address _resultCalculatorAddress){
+        resultDeclared = false;
+        ballotAddress = IBallot(_ballotAddress);
+        resultCalculatorAddress = IResultCalculator(_resultCalculatorAddress);
+    }
+
+    function addCandidate(uint256 _candidateId) public {
+        (bool success,) = address(ballotAddress).delegatecall(abi.encodeWithSignature("addCandidate(uint256)",_candidateId));
+        if (!success) {
+            revert Election__FailedToAddCandidate();
+        }
+        emit CandidateAdded(_candidateId);
+    }
+
+    function vote(uint256 _candidateId) public {
+        (bool success,) = address(ballotAddress).delegatecall(abi.encodeWithSignature("vote(uint256)",_candidateId));
+        if (!success) {
+            revert Election__FailedToAddCandidate();
+        }
+        emit Voted(_candidateId);
+    }
+
+    function getResults() public {
+        winnerId =  resultCalculatorAddress.calculateResult(address(ballotAddress));
+        emit ResultDeclared(winnerId);
+    }
+
+    function getWinner() public view returns (uint256) {
+        if(!resultDeclared){
+            revert Election__ResultsNotDeclared();
+        }
+        return winnerId;
+    }
     
 }
